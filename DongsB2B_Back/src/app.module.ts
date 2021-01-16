92ffa7674dcdcf026e0ecf8c8b05a1a70cbf8b93
jinn2u <bsybear623@gmail.com>
2021-01-16 23:50:46 +0900

    Test: DB connect, .env

--- DongsB2B_Back/src/app.module.ts
+++ DongsB2B_Back/src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import {TypeOrmModule} from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env" : ".test.env" 
+    }),
     GraphQLModule.forRoot({
       //자동으로 gql스키마파일을 생성한다.(true로 하면 메모리로부터 파일을 생성한다.)
       autoSchemaFile: true 
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'omyo',
+      password: '1234',
+      database: 'DongsB2B',
+      synchronize: true,
+      logging: true
     }),
     RestaurantsModule
   ],
