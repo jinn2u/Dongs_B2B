ae971e2f3a4522d8b73756df6f1457aeb5438694
jinn2u <bsybear623@gmail.com>
2021-01-17 00:44:00 +0900

    config .env

--- DongsB2B_Back/src/app.module.ts
+++ DongsB2B_Back/src/app.module.ts
@@ -3,13 +3,20 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import {TypeOrmModule} from '@nestjs/typeorm'
 import { ConfigModule } from '@nestjs/config';
-
-
+import * as Joi from 'joi'
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: process.env.NODE_ENV === "dev" ? ".dev.env" : ".test.env" 
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+      })
     }),
     GraphQLModule.forRoot({
       //자동으로 gql스키마파일을 생성한다.(true로 하면 메모리로부터 파일을 생성한다.)
@@ -18,10 +25,10 @@ import { ConfigModule } from '@nestjs/config';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
-      port: 5432,
-      username: 'omyo',
-      password: '1234',
-      database: 'DongsB2B',
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true
     }),
