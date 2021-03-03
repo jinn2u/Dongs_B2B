dda6684e51d676e6027dd40772aa8319591d42db
jinn2u <bsybear623@gmail.com>
2021-03-03 11:24:38 +0900

    [2e2test-users] createAccount

--- DongsB2B_Back/test/users.e2e-spec.ts
+++ DongsB2B_Back/test/users.e2e-spec.ts
@@ -4,6 +4,12 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+const GRAPHQL_ENDPOINT = '/graphql'
+jest.mock("got", () => {
+  return{
+    post: jest.fn()
+  }
+})
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -16,15 +22,60 @@ describe('UserModule (e2e)', () => {
     await app.init();
   });
 
-afterAll(()=> {
-  getConnection().dropDatabase()
-  app.close()
-})
+  afterAll(async () => {
+    await getConnection().dropDatabase();
+    app.close();
+  });
 
-it.todo('createAccount')
-it.todo('userProfile')
-it.todo('login')
-it.todo('me')
-it.todo('vertifyEmail')
-it.todo('editProfile')
+  describe('createAccount', () => {
+    const EMAIL = "bsybear623@gmail.com"
+
+    it('should create account', () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(true)
+        expect(res.body.data.createAccount.error).toBe(null)
+      })
+    })
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer()).post(GRAPHQL_ENDPOINT).send({
+        query: `
+          mutation {
+            createAccount(input: {
+              email:"${EMAIL}",
+              password:"12345",
+              role:Owner
+            }) {
+              ok
+              error
+            }
+          }
+        `,
+      })
+      .expect(200)
+      .expect(res => {
+        expect(res.body.data.createAccount.ok).toBe(false)
+        expect(res.body.data.createAccount.error).toBe("이메일을 가지고 있는 사용자가 존재합니다.")
+      })
+    })
+  })
+  it.todo('userProfile')
+  it.todo('login')
+  it.todo('me')
+  it.todo('vertifyEmail')
+  it.todo('editProfile')
 });
